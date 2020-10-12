241b6dd5d93beaf795e860de2aa5fa321782f4b5
james-desktop <ninjachimpstudios@gmail.com>
2020-10-12 13:40:25 +1000

    working

--- NCSStork/Main.cs
+++ NCSStork/Main.cs
@@ -1,11 +1,19 @@
 ﻿using System;
+using TaleWorlds.Core;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
 
 namespace NCSStork {
 
-    public class Main {
-
-        // todo
-
+    public class Main : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            Module.CurrentModule.AddInitialStateOption(new InitialStateOption("NCSMenuOption", new TextObject("NCS Menu", null), 9990, () =>
+            {
+                InformationManager.DisplayMessage(new InformationMessage("NCS Menu TODO"));
+            }, false));
+        }
     }
 
 }
